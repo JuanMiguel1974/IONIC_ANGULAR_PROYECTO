ecd034cc66b22395422a4bf8c9e63c6599365da6
JuanMiguel1974 <74014983+JuanMiguel1974@users.noreply.github.com>
2022-03-30 17:05:48 +0200

    IONIC_ANGULAR_PROYECTO

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,103 @@
+import { Injectable } from '@angular/core';
+import { User } from '../compartida/user';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
+import { Observable, of } from 'rxjs';
+import { switchMap} from 'rxjs/operators';
+import * as firebase from 'firebase/compat';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  public user$: Observable<User>;
+
+  constructor(private afAuth: AngularFireAuth , private afs: AngularFirestore) {
+    this.user$ = this.afAuth.authState.pipe(
+      switchMap((user) =>{
+        if(user){
+          return this.afs.doc<User>(`users/${user.uid}`).valueChanges();
+        }
+        return of(null);
+      })
+    );
+  }
+
+  async resetPassword(email: string): Promise<void> {
+    try {
+      return this.afAuth.sendPasswordResetEmail(email);
+    } catch (resetPasswordError) {
+      console.log('Error->', resetPasswordError);
+    }
+  }
+
+  async loginGoogle(): Promise<User> {
+    // eslint-disable-next-line prefer-const
+    try {
+      // eslint-disable-next-line prefer-const
+      let { user } = await this.afAuth.signInWithPopup(new firebase.default.auth.GoogleAuthProvider());
+      this.updateUserData(user);
+      return user;
+    } catch (logingGoogleError) {
+      console.log('Error->', logingGoogleError);
+    }
+  }
+
+  async register(email: string, password: string): Promise<User> {
+    try {
+      // eslint-disable-next-line prefer-const
+      let { user } = await this.afAuth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      await this.sendVerificationEmail();
+      return user;
+    } catch (registerError) {
+      console.log('Error->', registerError);
+    }
+  }
+
+  async login(email: string, password: string): Promise<User> {
+    try {
+      // eslint-disable-next-line prefer-const
+      let { user } = await this.afAuth.signInWithEmailAndPassword(
+        email,
+        password
+      );
+      this.updateUserData(user);
+      return user;
+    } catch (loginError) {
+      console.log('Error->', loginError);
+    }
+  }
+
+  async sendVerificationEmail(): Promise<void> {
+    try {
+      return (await this.afAuth.currentUser).sendEmailVerification();
+    } catch (sendEmailError) {
+      console.log('Error->', sendEmailError);
+    }
+  }
+
+  async logout(): Promise<void> {
+    try {
+      await this.afAuth.signOut();
+    } catch (logOutError) {
+      console.log('Error->', logOutError);
+    }
+  }
+
+  private updateUserData(user: User){
+    const userRef: AngularFirestoreDocument<User> = this.afs.doc(`users/${user.uid}`);
+
+    const data: User = {
+      uid: user.uid,
+      email: user.email,
+      emailVerified: user.emailVerified,
+      displayName: user.displayName,
+    };
+    return userRef.set(data, {merge: true});
+  }
+
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+ // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+firebaseConfig : {
+  apiKey: 'AIzaSyApr_5WOVSiBdmA-vSfCIs_3CjMfP4J2Rw',
+  authDomain: 'ionic-angular-ea1ae.firebaseapp.com',
+  projectId: 'ionic-angular-ea1ae',
+  storageBucket: 'ionic-angular-ea1ae.appspot.com',
+  messagingSenderId: '909949367208',
+  appId: '1:909949367208:web:c0e3d99e020ac1e119239d',
+  measurementId: 'G-MGTR8C9404'
+}
+
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
